49a281cd6ab4a968c11f10482c284ba5c76f11ef
BarbaraWachira <barbchira@gmail.com>
2023-04-04 09:04:32 -0700

    update2

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Represent a rectangle."""
 
-    def _init_(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
@@ -48,13 +48,13 @@ class Rectangle:
         """Return the perimeter of the Rectangle."""
         if self._width == 0 or self._height == 0:
             return (0)
-        return ((self._width * 2) + (self._height * 2))
+        return ((self.__width * 2) + (self.__height * 2))
 
-    def _str_(self):
+    def __str__(self):
         """Return the printable representation of the Rectangle.
         Represents the rectangle with the # character.
         """
-        if self._width == 0 or self._height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ("")
 
         rect = []
@@ -64,7 +64,7 @@ class Rectangle:
                 rect.append("\n")
         return ("".join(rect))
 
-    def _repr_(self):
+    def __repr__(self):
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
